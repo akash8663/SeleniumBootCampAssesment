942940616aaef4048ee86b52f5e87cd2449312bd
akash8663 <akash8663@gmail.com>
2017-06-18 05:41:56 -0400

    Test is running properly on both chrome and firefox

--- src/seleniumTask/AutomationHotel.java
+++ src/seleniumTask/AutomationHotel.java
@@ -1,5 +1,6 @@
 package seleniumTask;
 
+import static org.testng.Assert.assertEquals;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -8,6 +9,7 @@ import java.util.Date;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -15,51 +17,88 @@ public class AutomationHotel {
 
 	WebDriver driver;
 	Calendar calendar = Calendar.getInstance();
-	DateFormat dateFormat = new SimpleDateFormat("dd-LL-yyyy");
-	
-	@FindBy(name = "q-destination")
-	WebElement destinationName;
-	
-	@FindBy(name = "q-localised-check-in")
-	WebElement checkInDate;
-	
-	@FindBy(name = "q-localised-check-out")
-	WebElement checkOutDate;
-	
-	@FindBy(xpath = "//button[@class='cta cta-strong']")
-	WebElement searchBtn;
-	
-	public AutomationHotel (WebDriver driver) {
+	DateFormat dateFormat = new SimpleDateFormat("LL/dd/yy");
+
+	public AutomationHotel(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
-	public void enterDestinationName(String location) {
+
+	@FindBy(name = "q-destination")
+	WebElement destinationName;
+	@FindBy(css = "div.autosuggest-category-result")
+	WebElement autosuggesion;
+
+	public void enterDestinationName(String location) throws InterruptedException {
+		System.out.println("putting location");
+		destinationName.clear();
 		destinationName.sendKeys(location);
-		destinationName.submit();
+		Thread.sleep(2000);
+		autosuggesion.click();
+
 	}
-	public void enterCheckInDate() {
-		
+
+	@FindBy(name = "q-localised-check-in")
+	WebElement checkInDate;
+
+	public void enterCheckInDate() throws InterruptedException {
+		System.out.println("checkin date");
 		calendar.add(Calendar.DAY_OF_YEAR, 1);
 		Date tomorrow = calendar.getTime();
+		checkInDate.clear();
+		// Thread.sleep(2000);
 		checkInDate.sendKeys(dateFormat.format(tomorrow));
-		//System.out.println(dateFormat.format(tomorrow));
+		System.out.println(dateFormat.format(tomorrow));
+
 	}
-	public void enterCheckOutDate() {
-		
+
+	@FindBy(name = "q-localised-check-out")
+	WebElement checkOutDate;
+
+	public void enterCheckOutDate() throws InterruptedException {
+		System.out.println("checkout date");
 		calendar.add(Calendar.DAY_OF_YEAR, 5);
 		Date after5Days = calendar.getTime();
+		checkOutDate.clear();
+		// Thread.sleep(2000);
 		checkOutDate.sendKeys(dateFormat.format(after5Days));
-		//System.out.println(dateFormat.format(after5Days));
+		System.out.println(dateFormat.format(after5Days));
 	}
-	public void clickSearchBtn() {
-		searchBtn.click();
+
+	// @FindBy(xpath = "//button[@class='cta cta-strong']")
+	@FindBy(css = "button.cta.cta-strong")
+	WebElement searchBtn;
+
+	public void clickSearchBtn() throws InterruptedException {
+		System.out.println("clicking submit");
+		Thread.sleep(2000);
+		if (searchBtn.isDisplayed()) {
+			searchBtn.submit();
+		} else {
+			System.out.println("button not enable");
+		}
+		// checkOutDate.submit();
 	}
-	
-	public void searchingHotel(String location) {
+
+	// @FindBy(xpath = "//*[@class='summary']//*[text()='New York, New York,
+	// United States Of America']")
+	@FindBy(xpath = "//div[@class = 'summary']//h1")
+	WebElement verifyPage;
+
+	public void verifyNewPage() throws InterruptedException {
+		System.out.println("Navigating to next page");
+		Thread.sleep(5000);
+		System.out.println("Actual = " + verifyPage.getText());
+		assertEquals(verifyPage.getText(), "New York, New York, United States Of America");
+		// assertTrue(verifyPage.getText().equals("New York, New York, United
+		// States Of America"));
+	}
+
+	public void searchingHotel(String location) throws InterruptedException {
 		this.enterDestinationName(location);
 		this.enterCheckInDate();
 		this.enterCheckOutDate();
 		this.clickSearchBtn();
+		this.verifyNewPage();
 	}
 }
--- src/seleniumTaskTests/AutomationHotelTest.java
+++ src/seleniumTaskTests/AutomationHotelTest.java
@@ -4,38 +4,50 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import seleniumTask.AutomationHotel;
 
 public class AutomationHotelTest {
-	
+
 	WebDriver driver;
 	AutomationHotel hotel;
-	String driverPath = "D:\\SeleniumProjects\\WebDriver\\chromedriver_win32\\chromedriver.exe";
-	
+	String driverPathChrome = ".\\drivers\\chromedriver.exe";
+	String driverPathFirefox = ".\\drivers\\geckodriver.exe";
+
 	@BeforeTest
-	public void loadApp() {
-		System.setProperty("webdriver.chrome.driver", driverPath);
-		driver = new ChromeDriver();
-		
-		driver.get("https://www.hotels.com/");
+	@Parameters("browser")
+	public void loadApp(String browser) {
+
+		if (browser.equalsIgnoreCase("firefox")) {
+			// create firefox instance
+			System.setProperty("webdriver.gecko.driver", driverPathFirefox);
+			driver = new FirefoxDriver();
+		}
+		// Check if parameter passed as 'chrome'
+		else if (browser.equalsIgnoreCase("chrome")) {
+			// set path to chromedriver.exe
+			System.setProperty("webdriver.chrome.driver", driverPathChrome);
+			// create chrome instance
+			driver = new ChromeDriver();
+		}
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	
 	}
-	
+
 	@Test
-	public void testApp() {
+	public void testApp() throws InterruptedException {
+		driver.get("https://www.hotels.com/");
 		hotel = new AutomationHotel(driver);
 		hotel.searchingHotel("New York");
 	}
-	
-	
 
-	 
-	
-	
+	@AfterTest
+	public void exitingApp() {
+		driver.close();
+	}
 
 }
